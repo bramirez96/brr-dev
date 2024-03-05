e1fd7a07bbb4ead2bdfbf351765f5f1b22e1da48
bramirez96 <bran.ramirez.don@gmail.com>
2024-03-05 12:57:53 -0800

    Add in new dark mode automation helpers.

--- libs/ui/cozy-ui/src/providers/useDarkMode/useDarkMode.tsx
+++ libs/ui/cozy-ui/src/providers/useDarkMode/useDarkMode.tsx
@@ -2,8 +2,12 @@
 
 'use client';
 
+import { ReactNode, useContext, useEffect } from 'react';
 import { DarkModeContext, DarkModeContextProps } from './DarkModeContext';
 
+/**
+ * A simple hook to retrieve dark mode context values.
+ */
 export function useDarkMode(): DarkModeContextProps {
     const context = useContext(DarkModeContext);
 
@@ -14,4 +18,29 @@ export function useDarkMode(): DarkModeContextProps {
     }
 
     return context;
+}
+
+export type AutoDarkModeConfig = {
+    target?: HTMLElement;
+};
+
+/**
+ * A simple hook that will automatically trigger the dark mode class on the document body.
+ */
+export function useAutoDarkMode({
+    target = document.body,
+}: AutoDarkModeConfig = {}): void {
+    const { darkMode } = useDarkMode();
+
+    useEffect(() => {
+        target.classList.toggle('ui-dark-mode', darkMode);
+    }, [darkMode, target]);
+}
+
+/**
+ * A simple component with no UI that runs the useAutoDarkMode hook on your document.
+ */
+export function AutoDarkModeHelper(props: AutoDarkModeConfig = {}): ReactNode {
+    useAutoDarkMode(props);
+    return null;
 }
